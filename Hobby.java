54a1b7b0d38da021fb37afaa363264f643ab1ee8
christianmaligon-blip <christian.maligon@awsys-i.com>
2026-06-08 15:28:09 +0800

    Added 2nd mod

--- Hobby.java
+++ Hobby.java
@@ -4,4 +4,8 @@ public class Hobby {
 		displayTraineeHobbyB();
 		displayTraineeHobbyC();*/
 	}
+	public static void displayTraineeHobbyX() {
+		System.out.println("Trainee X’s Hobbies:");
+		// Hobbies of Trainee X will be added here
+	}
 }
